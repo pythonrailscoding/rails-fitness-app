b6134896b3e28e650f38e8a650760f833ecc9e7c
umesh <umeshpurohit1975@gmail.com>
2021-06-24 23:51:39 +0530

    improved ui and added more functionality

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,8 @@
 class Entry < ApplicationRecord
+
+  validates :calories, :carbohydrates, :proteins, :fats, :meal_type, presence: true
+
+  def day
+    self.created_at.strftime("%b %e, %Y")
+  end
 end
--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,1 +1,4 @@
+# Be sure to restart your server when modified this file
+# This file formats the date and displays it as DD M, YYYY as 22 January, 2021
+
 Time::DATE_FORMATS[:nice_date] = "%B %e, %Y"
